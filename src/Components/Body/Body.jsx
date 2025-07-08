6caa0b22706687983b1336956eab53e1a4d8084e
Mit Mehta <mitrmehta20@gmail.com>
2025-07-08 02:44:31 -0400

    milestone 3

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -4,6 +4,7 @@ import Home from './Home/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Milestone1 from './Milestones/Milestone1/Milestone1'
 import Milestone2 from './Milestones/Milestone2/Milestone2';
+import Milestone3 from './Milestones/Milestone3/Milestone3';
 
 const Body = () => {
   return (
@@ -12,6 +13,7 @@ const Body = () => {
                 <Route path='/' element={<Home/>}/>
                 <Route path='/milestone1' element={<Milestone1/>}/>
                 <Route path='/milestone2' element={<Milestone2/>}/>
+                <Route path='/milestone3' element={<Milestone3/>}/>
             </Routes>
 
         </div>
